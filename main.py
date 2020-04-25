92c3746bc7b272fc55ab04554c46f9a2c09bfa18
BoruKun <raskin.evgeniy@mail.ru>
2020-04-25 15:33:58 +0500

    Repo init

--- main.py
+++ main.py
@@ -7,7 +7,37 @@ https://www.kaggle.com/rounakbanik/pokemon
 """
 
 import pandas
-import sklearn
 
-# Load our data
-data = pandas.read_csv("pokemon.csv", delimeter=",")
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+
+def main():
+    # Load our data
+    data = pandas.read_csv("pokemon.csv")
+
+    # Organize our data
+    X = data.drop(columns = ["is_legendary"])
+    y = data["is_legendary"]
+
+    # Split our data to test and train samples
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.3, random_state=42)
+
+    # Handle features
+    integer_values = X.select_dtypes(include=['integer']).columns.to_list()
+
+    # Initialize our classifier
+    gnb = GaussianNB()
+
+    # Train
+    model = gnb.fit(X_train[integer_values], y_train)
+
+    # Predict
+    predictions = gnb.predict(X_test[integer_values])
+    print(predictions)
+
+
+if __name__ == "__main__":
+    main()
